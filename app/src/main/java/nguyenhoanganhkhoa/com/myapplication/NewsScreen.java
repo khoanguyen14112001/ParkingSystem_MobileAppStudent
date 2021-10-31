cbb334ff23cc07604add0df7568ddac766db2ff8
trhgy761 <yth19411c@st.uel.edu.vn>
2021-10-31 23:37:09 +0700

    Code giao diện NEWS

---
+++ app/src/main/java/nguyenhoanganhkhoa/com/myapplication/NewsScreen.java
@@ -0,0 +1,14 @@
+package nguyenhoanganhkhoa.com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_screen);
+    }
+}
