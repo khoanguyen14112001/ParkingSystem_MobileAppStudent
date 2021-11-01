5df6af6633bfe300a1956e994b2a9e72ebfe897d
trhgy761 <yth19411c@st.uel.edu.vn>
2021-11-01 13:12:34 +0700

    Code giao diện Help center

---
+++ app/src/main/java/nguyenhoanganhkhoa/com/myapplication/HelpCenterScreen.java
@@ -0,0 +1,14 @@
+package nguyenhoanganhkhoa.com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpCenterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_center_screen);
+    }
+}
