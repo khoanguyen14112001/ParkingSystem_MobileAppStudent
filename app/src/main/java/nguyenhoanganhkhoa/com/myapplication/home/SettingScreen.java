413120450c571c82481dc4b905c664a3faa8c323
trhgy761 <yth19411c@st.uel.edu.vn>
2021-11-02 00:40:18 +0700

    Code giao diện Setting

---
+++ app/src/main/java/nguyenhoanganhkhoa/com/myapplication/home/SettingScreen.java
@@ -0,0 +1,16 @@
+package nguyenhoanganhkhoa.com.myapplication.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nguyenhoanganhkhoa.com.myapplication.R;
+
+public class SettingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+    }
+}
